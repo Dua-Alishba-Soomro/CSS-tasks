b7fb7ab5fd5e58fa8de6ab13b3a0250ad23079fc
Dua-Alishba-Soomro <dua.alishba111@gmail.com>
2025-03-03 23:12:05 +0500

    Add files via upload

---
+++ java16.js
@@ -0,0 +1,49 @@
+// Arrays
+
+// Printing the vehicals 
+
+//let vehicals = ["truck", "car", "bike", "darsan", "raksha", "trailor "];
+//let hint = false;
+
+//console.log(vehicals[1])
+//console.log(vehicals[4])
+//console.log(vehicals.length)
+
+//for (i = 0; i < vehicals.length ; i++){
+ //   console.log(vehicals[i]) }
+
+ //for (i = 0; i < vehicals.length ; i++){
+ //   if(vehicals[i] == "bike"){
+  //     hint = true;
+
+// console.log("Bike is avilable ") 
+
+
+//} else{
+//    console.log(" Bike is not avilable ")}   } 
+   
+//if(hint){
+ // console.log(" The element is avilable in this array ")
+//}
+//else{
+//    console.log("The element is not avilable")
+//}
+
+/****************************************************************************************/
+
+// sum of even NUMBERS
+/* let number = [2, 4, 6, 5, 14, 13, 13, 3, 31 ];
+let sum = 0;
+for(let i = 0; i < number.length; i++){
+    if(number[i] % 2 == 0){
+        //console.log(number[i]);
+        sum = sum + number[i]
+    }
+}
+
+console.log(sum); */
+
+/****************************************************************************************/
+
+let car = ["Corola ", "F41-123", 2020 , "black", 2000000 ];
+console.log(car);
